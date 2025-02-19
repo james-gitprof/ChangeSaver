fb0b5a215da79ada55f825229c0e7b61c5836082
james <wepudaljamesbernard05@gmail.com>
2025-02-19 17:32:57 +0800

    added initial nav design

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
 import "./main.css";
+import { Icons } from "./icons";
 
+const hamburgerElement = document.getElementById("mobile-nav-menu") as HTMLSpanElement;
+
+class App {
+
+  start() {
+    this.initializeContents();
+  }
+
+  initializeContents() {
+    hamburgerElement.innerHTML = Icons.SVG_HAMBURGER;
+  }
+}
+
+const app = new App();
+app.start();
